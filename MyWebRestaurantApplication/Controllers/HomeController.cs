2449f1957949ebdda3b31a68de7b503361a70efb
IvailoDavidov <ivailo.davidov@abv.bg>
2022-04-10 16:15:30 +0300

    Clearing Some Data

--- MyWebRestaurantApplication/Controllers/HomeController.cs
+++ MyWebRestaurantApplication/Controllers/HomeController.cs
@@ -31,6 +31,11 @@ namespace MyWebRestaurantApplication.Controllers
             return View();
         }
 
+        public IActionResult Maintenance()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
